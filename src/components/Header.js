3cfde7e929e0285ce2893cee85c70abb69cda3a8
alex5667 <alex56671111@gmail.com>
2023-05-03 07:00:35 +0300

    feat: add link

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,8 @@ import { render } from "../js/utils/render.js";
 export const Header = () => {
   const title = render('h1', 'header__title', 'RSS Virtual Keyboard');
   const note = render('p', 'header__note', 'Use left <kbd>Ctrl</kbd> + <kbd>Alt</kbd> to change language.');
-  const header = render('header', 'header__container', [title, note]);
+  const link = render('p', 'header__link', 'Link to the pool-request: <a  class ="header__link" href="https://github.com/alex5667/virtual-keyboard/pull/1">https://github.com/alex5667/virtual-keyboard/pull/1</a>');
+  const header = render('header', 'header__container', [title, note, link]);
   document.body.appendChild(header)
   return header;
 }
